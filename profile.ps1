71d3f3c141802faff19585c8a1ed35d47a1c193b
super50nic <119553870+super50nic@users.noreply.github.com>
2025-11-17 17:23:05 -0800

    Add files via upload

---
+++ profile.ps1
@@ -0,0 +1,13 @@
+# Azure Functions profile.ps1
+#
+# This profile.ps1 will get executed every "cold start" of your Function App.
+# "cold start" occurs when:
+#
+# * A Function App starts up for the very first time
+# * A Function App starts up after being de-allocated due to inactivity
+#
+# You can define helper functions, run commands, or specify environment variables
+# NOTE: any variables defined that are not environment variables will get reset after the first execution
+
+# You can define helper functions, run commands, or specify environment variables here.
+# These will be available to all functions in the Function App during the current session.
